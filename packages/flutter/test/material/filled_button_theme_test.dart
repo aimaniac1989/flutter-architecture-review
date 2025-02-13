flutter/flutter

commit 51ed348f3bfd911e15de24174b0117b525ff533a

Fix handling of `iconSize` and `iconColor` defaults for `ButtonStyleButton` subclasses. (#143501)
## Description

Adds defaults that use tokens to define default `iconSize` and `iconColor` values. Previously, the Material 3 token values for button icon sizes and colors were not being used as defaults when the `ButtonStyleButton.defaultStyleOf` function returned the default values.

Adds tests to make sure appropriate `ButtonStyle` fields are populated when defaultStyle is called on buttons.

Updated documentation for `defaultStyleOf` to indicated that not _all_ fields need to be non-null, since some fields make sense to be null (e.g. `fixedSize`) because they would otherwise override the behavior of other fields in the same `ButtonStyle`.

## Tests
 - Added tests to make sure that the appropriate fields are non-null in the default button styles for each type of button.

packages/flutter/lib/src/material/button_style_button.dart
@@ -162,22 +162,39 @@ abstract class ButtonStyleButton extends StatefulWidget {
   /// {@macro flutter.material.ButtonStyleButton.iconAlignment}
   final IconAlignment iconAlignment;
 
-  /// Returns a non-null [ButtonStyle] that's based primarily on the [Theme]'s
-  /// [ThemeData.textTheme] and [ThemeData.colorScheme].
+  /// Returns a [ButtonStyle] that's based primarily on the [Theme]'s
+  /// [ThemeData.textTheme] and [ThemeData.colorScheme], but has most values
+  /// filled out (non-null).
   ///
-  /// The returned style can be overridden by the [style] parameter and
-  /// by the style returned by [themeStyleOf]. For example the default
-  /// style of the [TextButton] subclass can be overridden with its
-  /// [TextButton.style] constructor parameter, or with a
-  /// [TextButtonTheme].
+  /// The returned style can be overridden by the [style] parameter and by the
+  /// style returned by [themeStyleOf] that some button-specific themes like
+  /// [TextButtonTheme] or [ElevatedButtonTheme] override. For example the
+  /// default style of the [TextButton] subclass can be overridden with its
+  /// [TextButton.style] constructor parameter, or with a [TextButtonTheme].
   ///
-  /// Concrete button subclasses should return a ButtonStyle that
-  /// has no null properties, and where all of the [WidgetStateProperty]
-  /// properties resolve to non-null values.
+  /// Concrete button subclasses should return a [ButtonStyle] with as many
+  /// non-null properties as possible, where all of the non-null
+  /// [WidgetStateProperty] properties resolve to non-null values.
+  ///
+  /// ## Properties that can be null
+  ///
+  /// Some properties, like [ButtonStyle.fixedSize] would override other values
+  /// in the same [ButtonStyle] if set, so they are allowed to be null.  Here is
+  /// a summary of properties that are allowed to be null when returned in the
+  /// [ButtonStyle] returned by this function, an why:
+  ///
+  /// - [ButtonStyle.fixedSize] because it would override other values in the
+  ///   same [ButtonStyle], like [ButtonStyle.maximumSize].
+  /// - [ButtonStyle.side] because null is a valid value for a button that has
+  ///   no side. [OutlinedButton] returns a non-null default for this, however.
+  /// - [ButtonStyle.backgroundBuilder] and [ButtonStyle.foregroundBuilder]
+  ///   because they would override the [ButtonStyle.foregroundColor] and
+  ///   [ButtonStyle.backgroundColor] of the same [ButtonStyle].
   ///
   /// See also:
   ///
-  ///  * [themeStyleOf], Returns the ButtonStyle of this button's component theme.
+  /// * [themeStyleOf], returns the ButtonStyle of this button's component
+  ///   theme.
   @protected
   ButtonStyle defaultStyleOf(BuildContext context);
 
@@ -479,7 +496,10 @@ class _ButtonStyleState extends State<ButtonStyleButton> with TickerProviderStat
           customBorder: resolvedShape.copyWith(side: resolvedSide),
           statesController: statesController,
           child: IconTheme.merge(
-            data: IconThemeData(color: resolvedIconColor ?? resolvedForegroundColor, size: resolvedIconSize),
+            data: IconThemeData(
+              color: resolvedIconColor ?? resolvedForegroundColor,
+              size: resolvedIconSize,
+            ),
             child: effectiveChild,
           ),
         ),

packages/flutter/lib/src/material/elevated_button.dart
@@ -699,6 +699,29 @@ class _ElevatedButtonDefaultsM3 extends ButtonStyle {
   // No default fixedSize
 
   @override
+  MaterialStateProperty<double>? get iconSize =>
+    const MaterialStatePropertyAll<double>(18.0);
+
+  @override
+  MaterialStateProperty<Color>? get iconColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return _colors.onSurface.withOpacity(0.38);
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.focused)) {
+        return _colors.primary;
+      }
+      return _colors.primary;
+    });
+  }
+
+  @override
   MaterialStateProperty<Size>? get maximumSize =>
     const MaterialStatePropertyAll<Size>(Size.infinite);
 

packages/flutter/lib/src/material/filled_button.dart
@@ -731,6 +731,29 @@ class _FilledButtonDefaultsM3 extends ButtonStyle {
   // No default fixedSize
 
   @override
+  MaterialStateProperty<double>? get iconSize =>
+    const MaterialStatePropertyAll<double>(18.0);
+
+  @override
+  MaterialStateProperty<Color>? get iconColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return _colors.onSurface.withOpacity(0.38);
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return _colors.onPrimary;
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return _colors.onPrimary;
+      }
+      if (states.contains(MaterialState.focused)) {
+        return _colors.onPrimary;
+      }
+      return _colors.onPrimary;
+    });
+  }
+
+  @override
   MaterialStateProperty<Size>? get maximumSize =>
     const MaterialStatePropertyAll<Size>(Size.infinite);
 
@@ -851,6 +874,29 @@ class _FilledTonalButtonDefaultsM3 extends ButtonStyle {
     const MaterialStatePropertyAll<Size>(Size(64.0, 40.0));
 
   // No default fixedSize
+
+  @override
+  MaterialStateProperty<double>? get iconSize =>
+    const MaterialStatePropertyAll<double>(18.0);
+
+  @override
+  MaterialStateProperty<Color>? get iconColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return _colors.onSurface.withOpacity(0.38);
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return _colors.onSecondaryContainer;
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return _colors.onSecondaryContainer;
+      }
+      if (states.contains(MaterialState.focused)) {
+        return _colors.onSecondaryContainer;
+      }
+      return _colors.onSecondaryContainer;
+    });
+  }
 
   @override
   MaterialStateProperty<Size>? get maximumSize =>

packages/flutter/lib/src/material/outlined_button.dart
@@ -607,6 +607,29 @@ class _OutlinedButtonDefaultsM3 extends ButtonStyle {
   // No default fixedSize
 
   @override
+  MaterialStateProperty<double>? get iconSize =>
+    const MaterialStatePropertyAll<double>(18.0);
+
+  @override
+  MaterialStateProperty<Color>? get iconColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return _colors.onSurface.withOpacity(0.38);
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.focused)) {
+        return _colors.primary;
+      }
+      return _colors.primary;
+    });
+  }
+
+  @override
   MaterialStateProperty<Size>? get maximumSize =>
     const MaterialStatePropertyAll<Size>(Size.infinite);
 

packages/flutter/lib/src/material/text_button.dart
@@ -640,6 +640,29 @@ class _TextButtonDefaultsM3 extends ButtonStyle {
   // No default fixedSize
 
   @override
+  MaterialStateProperty<double>? get iconSize =>
+    const MaterialStatePropertyAll<double>(18.0);
+
+  @override
+  MaterialStateProperty<Color>? get iconColor {
+    return MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+      if (states.contains(MaterialState.disabled)) {
+        return _colors.onSurface.withOpacity(0.38);
+      }
+      if (states.contains(MaterialState.pressed)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.hovered)) {
+        return _colors.primary;
+      }
+      if (states.contains(MaterialState.focused)) {
+        return _colors.primary;
+      }
+      return _colors.primary;
+    });
+  }
+
+  @override
   MaterialStateProperty<Size>? get maximumSize =>
     const MaterialStatePropertyAll<Size>(Size.infinite);
 

aimaniac1989/flutter-architecture-review

commit 469092a2d15d1c54127bbaf860e9c2534ec8774a

Fix buttons with icons ignore provided `foregroundColor` (#162880)
Fixes [Flutter 3.27 and later breaks past styling and theming of icon
color on buttons with
icons](https://github.com/flutter/flutter/issues/162839)

### Description

This PR fixes how the icon color is resolved in `ButtonStyleButton`.
This was regressed in https://github.com/flutter/flutter/pull/143501.

### Code Sample (taken from issue)

<details>
<summary>expand to view the code sample</summary> 

```dart
import 'package:flutter/material.dart';

void main() => runApp(const MyApp());

final ButtonStyle filledButtonStyle = FilledButton.styleFrom(
  foregroundColor: Colors.red,
  backgroundColor: Colors.grey,
);

final ButtonStyle elevatedButtonStyle = ElevatedButton.styleFrom(
  foregroundColor: Colors.orange.shade600,
  backgroundColor: Colors.blueGrey,
);

final ButtonStyle outlinedButtonStyle = OutlinedButton.styleFrom(
  foregroundColor: Colors.lightBlue,
);

final ButtonStyle textButtonStyle = TextButton.styleFrom(
  foregroundColor: Colors.green,
);

final ButtonStyle segmentedButtonStyle = SegmentedButton.styleFrom(
  selectedForegroundColor: Colors.tealAccent.shade700,
);

class MyApp extends StatelessWidget {
  const MyApp({super.key});

  @override
  Widget build(BuildContext context) {
    return MaterialApp(
      debugShowCheckedModeBanner: false,
      home: const HomePage(),
      theme: ThemeData(
        filledButtonTheme: FilledButtonThemeData(
          style: filledButtonStyle,
        ),
        elevatedButtonTheme: ElevatedButtonThemeData(
          style: elevatedButtonStyle,
        ),
        outlinedButtonTheme: OutlinedButtonThemeData(
          style: outlinedButtonStyle,
        ),
        textButtonTheme: TextButtonThemeData(
          style: textButtonStyle,
        ),
        segmentedButtonTheme: SegmentedButtonThemeData(
          style: segmentedButtonStyle,
        ),
      ),
    );
  }
}

class HomePage extends StatelessWidget {
  const HomePage({super.key});

packages/flutter/lib/src/material/button_style_button.dart
@@ -389,6 +389,16 @@ class _ButtonStyleState extends State<ButtonStyleButton> with TickerProviderStat
       });
     }
 
+    Color? effectiveIconColor() {
+      return widgetStyle?.iconColor?.resolve(statesController.value) ??
+          themeStyle?.iconColor?.resolve(statesController.value) ??
+          widgetStyle?.foregroundColor?.resolve(statesController.value) ??
+          themeStyle?.foregroundColor?.resolve(statesController.value) ??
+          defaultStyle.iconColor?.resolve(statesController.value) ??
+          // Fallback to foregroundColor if iconColor is null.
+          defaultStyle.foregroundColor?.resolve(statesController.value);
+    }
+
     final double? resolvedElevation = resolve<double?>((ButtonStyle? style) => style?.elevation);
     final TextStyle? resolvedTextStyle = resolve<TextStyle?>(
       (ButtonStyle? style) => style?.textStyle,
@@ -409,7 +419,7 @@ class _ButtonStyleState extends State<ButtonStyleButton> with TickerProviderStat
     final Size? resolvedMinimumSize = resolve<Size?>((ButtonStyle? style) => style?.minimumSize);
     final Size? resolvedFixedSize = resolve<Size?>((ButtonStyle? style) => style?.fixedSize);
     final Size? resolvedMaximumSize = resolve<Size?>((ButtonStyle? style) => style?.maximumSize);
-    final Color? resolvedIconColor = resolve<Color?>((ButtonStyle? style) => style?.iconColor);
+    final Color? resolvedIconColor = effectiveIconColor();
     final double? resolvedIconSize = resolve<double?>((ButtonStyle? style) => style?.iconSize);
     final BorderSide? resolvedSide = resolve<BorderSide?>((ButtonStyle? style) => style?.side);
     final OutlinedBorder? resolvedShape = resolve<OutlinedBorder?>(
@@ -551,10 +561,7 @@ class _ButtonStyleState extends State<ButtonStyleButton> with TickerProviderStat
       customBorder: resolvedShape!.copyWith(side: resolvedSide),
       statesController: statesController,
       child: IconTheme.merge(
-        data: IconThemeData(
-          color: resolvedIconColor ?? resolvedForegroundColor,
-          size: resolvedIconSize,
-        ),
+        data: IconThemeData(color: resolvedIconColor, size: resolvedIconSize),
         child: result,
       ),
     );

packages/flutter/lib/src/material/elevated_button.dart
@@ -161,6 +161,9 @@ class ElevatedButton extends ButtonStyleButton {
   /// [ButtonStyle.iconColor] and [iconSize] is used to construct
   /// [ButtonStyle.iconSize].
   ///
+  /// If [iconColor] is null, the button icon will use [foregroundColor]. If [foregroundColor] is also
+  /// null, the button icon will use the default icon color.
+  ///
   /// The button's elevations are defined relative to the [elevation]
   /// parameter. The disabled elevation is the same as the parameter
   /// value, [elevation] + 2 is used when the button is hovered

packages/flutter/lib/src/material/filled_button.dart
@@ -232,6 +232,9 @@ class FilledButton extends ButtonStyleButton {
   /// [ButtonStyle.iconColor] and [iconSize] is used to construct
   /// [ButtonStyle.iconSize].
   ///
+  /// If [iconColor] is null, the button icon will use [foregroundColor]. If [foregroundColor] is also
+  /// null, the button icon will use the default icon color.
+  ///
   /// The button's elevations are defined relative to the [elevation]
   /// parameter. The disabled elevation is the same as the parameter
   /// value, [elevation] + 2 is used when the button is hovered

packages/flutter/lib/src/material/outlined_button.dart
@@ -160,6 +160,9 @@ class OutlinedButton extends ButtonStyleButton {
   /// [ButtonStyle.iconColor] and [iconSize] is used to construct
   /// [ButtonStyle.iconSize].
   ///
+  /// If [iconColor] is null, the button icon will use [foregroundColor]. If [foregroundColor] is also
+  /// null, the button icon will use the default icon color.
+  ///
   /// If [overlayColor] is specified and its value is [Colors.transparent]
   /// then the pressed/focused/hovered highlights are effectively defeated.
   /// Otherwise a [WidgetStateProperty] with the same opacities as the

packages/flutter/lib/src/material/text_button.dart
@@ -169,6 +169,9 @@ class TextButton extends ButtonStyleButton {
   /// [ButtonStyle.iconColor] and [iconSize] is used to construct
   /// [ButtonStyle.iconSize].
   ///
+  /// If [iconColor] is null, the button icon will use [foregroundColor]. If [foregroundColor] is also
+  /// null, the button icon will use the default icon color.
+  ///
   /// If [overlayColor] is specified and its value is [Colors.transparent]
   /// then the pressed/focused/hovered highlights are effectively defeated.
   /// Otherwise a [WidgetStateProperty] with the same opacities as the

packages/flutter/test/material/elevated_button_test.dart
@@ -2510,4 +2510,27 @@ void main() {
     final Offset iconTopRight = tester.getTopRight(find.byIcon(Icons.add));
     expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets('ElevatedButton icon uses provided foregroundColor over default icon color', (
+    WidgetTester tester,
+  ) async {
+    const Color foregroundColor = Color(0xFFFF1234);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(foregroundColor: foregroundColor),
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Button'),
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(iconStyle(tester, Icons.add).color, foregroundColor);
+  });
 }

packages/flutter/test/material/elevated_button_theme_test.dart
@@ -6,6 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TextStyle iconStyle(WidgetTester tester, IconData icon) {
+    final RichText iconRichText = tester.widget<RichText>(
+      find.descendant(of: find.byIcon(icon), matching: find.byType(RichText)),
+    );
+    return iconRichText.text.style!;
+  }
+
   test('ElevatedButtonThemeData lerp special cases', () {
     expect(ElevatedButtonThemeData.lerp(null, null, 0), null);
     const ElevatedButtonThemeData data = ElevatedButtonThemeData();
@@ -263,7 +270,7 @@ void main() {
     );
   });
 
-  testWidgets('Material3 - ElevatedButton repsects Theme shadowColor', (WidgetTester tester) async {
+  testWidgets('Material3 - ElevatedButton respects Theme shadowColor', (WidgetTester tester) async {
     const ColorScheme colorScheme = ColorScheme.light();
     const Color shadowColor = Color(0xff000001);
     const Color overriddenColor = Color(0xff000002);
@@ -334,7 +341,7 @@ void main() {
     expect(material.shadowColor, shadowColor);
   });
 
-  testWidgets('Material2 - ElevatedButton repsects Theme shadowColor', (WidgetTester tester) async {
+  testWidgets('Material2 - ElevatedButton respects Theme shadowColor', (WidgetTester tester) async {
     const ColorScheme colorScheme = ColorScheme.light();
     const Color shadowColor = Color(0xff000001);
     const Color overriddenColor = Color(0xff000002);
@@ -442,4 +449,33 @@ void main() {
 
     expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets(
+    'ElevatedButton icon uses provided ElevatedButtonTheme foregroundColor over default icon color',
+    (WidgetTester tester) async {
+      const Color foregroundColor = Color(0xFFFFA500);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(foregroundColor: foregroundColor),
+            ),
+          ),
+          home: Material(
+            child: Center(
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('Button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(iconStyle(tester, Icons.add).color, foregroundColor);
+    },
+  );
 }

packages/flutter/test/material/filled_button_test.dart
@@ -2756,4 +2756,38 @@ void main() {
     final Offset iconTopRight = tester.getTopRight(find.byIcon(Icons.add));
     expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets('FilledButton icon uses provided foregroundColor over default icon color', (
+    WidgetTester tester,
+  ) async {
+    const Color foregroundColor = Color(0xFFFF1234);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                FilledButton.icon(
+                  style: FilledButton.styleFrom(foregroundColor: foregroundColor),
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text('Button'),
+                ),
+                FilledButton.tonalIcon(
+                  style: FilledButton.styleFrom(foregroundColor: foregroundColor),
+                  onPressed: () {},
+                  icon: const Icon(Icons.mail),
+                  label: const Text('Button'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(iconStyle(tester, Icons.add).color, foregroundColor);
+    expect(iconStyle(tester, Icons.mail).color, foregroundColor);
+  });
 }

packages/flutter/test/material/filled_button_theme_test.dart
@@ -6,6 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TextStyle iconStyle(WidgetTester tester, IconData icon) {
+    final RichText iconRichText = tester.widget<RichText>(
+      find.descendant(of: find.byIcon(icon), matching: find.byType(RichText)),
+    );
+    return iconRichText.text.style!;
+  }
+
   test('FilledButtonThemeData lerp special cases', () {
     expect(FilledButtonThemeData.lerp(null, null, 0), null);
     const FilledButtonThemeData data = FilledButtonThemeData();
@@ -360,4 +367,43 @@ void main() {
 
     expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets(
+    'FilledButton icon uses provided FilledButtonTheme foregroundColor over default icon color',
+    (WidgetTester tester) async {
+      const Color foregroundColor = Color(0xFFFFA500);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            filledButtonTheme: FilledButtonThemeData(
+              style: FilledButton.styleFrom(foregroundColor: foregroundColor),
+            ),
+          ),
+          home: Material(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  FilledButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.add),
+                    label: const Text('Button'),
+                  ),
+                  FilledButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Icons.mail),
+                    label: const Text('Button'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(iconStyle(tester, Icons.add).color, foregroundColor);
+      expect(iconStyle(tester, Icons.mail).color, foregroundColor);
+    },
+  );
 }

packages/flutter/test/material/outlined_button_test.dart
@@ -2854,4 +2854,27 @@ void main() {
     final Offset iconTopRight = tester.getTopRight(find.byIcon(Icons.add));
     expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets('OutlinedButton icon uses provided foregroundColor over default icon color', (
+    WidgetTester tester,
+  ) async {
+    const Color foregroundColor = Color(0xFFFF1234);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: OutlinedButton.icon(
+              style: OutlinedButton.styleFrom(foregroundColor: foregroundColor),
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Button'),
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(iconStyle(tester, Icons.add).color, foregroundColor);
+  });
 }

packages/flutter/test/material/outlined_button_theme_test.dart
@@ -6,6 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TextStyle iconStyle(WidgetTester tester, IconData icon) {
+    final RichText iconRichText = tester.widget<RichText>(
+      find.descendant(of: find.byIcon(icon), matching: find.byType(RichText)),
+    );
+    return iconRichText.text.style!;
+  }
+
   test('OutlinedButtonThemeData lerp special cases', () {
     expect(OutlinedButtonThemeData.lerp(null, null, 0), null);
     const OutlinedButtonThemeData data = OutlinedButtonThemeData();
@@ -444,6 +451,35 @@ void main() {
       final Offset iconTopRight = tester.getTopRight(find.byIcon(Icons.add));
 
       expect(buttonTopRight.dx, iconTopRight.dx + 24.0);
+    },
+  );
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets(
+    'OutlinedButton icon uses provided OutlinedButtonTheme foregroundColor over default icon color',
+    (WidgetTester tester) async {
+      const Color foregroundColor = Color(0xFFFFA500);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            outlinedButtonTheme: OutlinedButtonThemeData(
+              style: OutlinedButton.styleFrom(foregroundColor: foregroundColor),
+            ),
+          ),
+          home: Material(
+            child: Center(
+              child: OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('Button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(iconStyle(tester, Icons.add).color, foregroundColor);
     },
   );
 }

packages/flutter/test/material/text_button_test.dart
@@ -2547,4 +2547,27 @@ void main() {
     final Offset iconTopRight = tester.getTopRight(find.byIcon(Icons.add));
     expect(buttonTopRight.dx, iconTopRight.dx + 16.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets('TextButton icon uses provided foregroundColor over default icon color', (
+    WidgetTester tester,
+  ) async {
+    const Color foregroundColor = Color(0xFFFF1234);
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Material(
+          child: Center(
+            child: TextButton.icon(
+              style: TextButton.styleFrom(foregroundColor: foregroundColor),
+              onPressed: () {},
+              icon: const Icon(Icons.add),
+              label: const Text('Button'),
+            ),
+          ),
+        ),
+      ),
+    );
+    expect(iconStyle(tester, Icons.add).color, foregroundColor);
+  });
 }

packages/flutter/test/material/text_button_theme_test.dart
@@ -6,6 +6,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  TextStyle iconStyle(WidgetTester tester, IconData icon) {
+    final RichText iconRichText = tester.widget<RichText>(
+      find.descendant(of: find.byIcon(icon), matching: find.byType(RichText)),
+    );
+    return iconRichText.text.style!;
+  }
+
   test('TextButtonTheme lerp special cases', () {
     expect(TextButtonThemeData.lerp(null, null, 0), null);
     const TextButtonThemeData data = TextButtonThemeData();
@@ -447,4 +454,33 @@ void main() {
 
     expect(buttonTopRight.dx, iconTopRight.dx + 16.0);
   });
+
+  // Regression test for https://github.com/flutter/flutter/issues/162839.
+  testWidgets(
+    'TextButton icon uses provided TextButtonThemeData foregroundColor over default icon color',
+    (WidgetTester tester) async {
+      const Color foregroundColor = Color(0xFFFFA500);
+
+      await tester.pumpWidget(
+        MaterialApp(
+          theme: ThemeData(
+            textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(foregroundColor: foregroundColor),
+            ),
+          ),
+          home: Material(
+            child: Center(
+              child: TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text('Button'),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      expect(iconStyle(tester, Icons.add).color, foregroundColor);
+    },
+  );
 }