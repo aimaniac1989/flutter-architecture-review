aimaniac1989/flutter-architecture-review

commit bb116cb55b53ffcaaf61e18c2359ad36bf2f2589

Properly lay out and position RenderWebImage (#171916)
Fixes https://github.com/flutter/flutter/issues/163288
Fixes https://github.com/flutter/flutter/issues/164405

The previous behavior of `RenderWebImage` for layout and positioning did
not work for all values of `BoxFit` and `AlignmentGeometry`. This PR
gets `RenderWebImage` to properly lay out and offset its child (the
`<img>` element).

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [x] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [x] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
[test-exempt]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
[Flutter Style Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md
[Features we expect every widget to implement]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Style-guide-for-Flutter-repo.md#features-we-expect-every-widget-to-implement
[CLA]: https://cla.developers.google.com/
[flutter/tests]: https://github.com/flutter/tests
[breaking change policy]:

packages/flutter/lib/src/painting/_network_image_io.dart
@@ -164,13 +164,16 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
     if (other.runtimeType != runtimeType) {
       return false;
     }
-    return other is NetworkImage && other.url == url && other.scale == scale;
+    return other is NetworkImage &&
+        other.url == url &&
+        other.scale == scale &&
+        mapEquals(other.headers, headers);
   }
 
   @override
-  int get hashCode => Object.hash(url, scale);
+  int get hashCode => Object.hash(url, scale, headers);
 
   @override
   String toString() =>
-      '${objectRuntimeType(this, 'NetworkImage')}("$url", scale: ${scale.toStringAsFixed(1)})';
+      '${objectRuntimeType(this, 'NetworkImage')}("$url", scale: ${scale.toStringAsFixed(1)}, webHtmlElementStrategy: ${webHtmlElementStrategy.name}, headers: $headers)';
 }

packages/flutter/lib/src/painting/_network_image_web.dart
@@ -242,15 +242,19 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
     if (other.runtimeType != runtimeType) {
       return false;
     }
-    return other is NetworkImage && other.url == url && other.scale == scale;
+    return other is NetworkImage &&
+        other.url == url &&
+        other.scale == scale &&
+        other.webHtmlElementStrategy == webHtmlElementStrategy &&
+        mapEquals(other.headers, headers);
   }
 
   @override
-  int get hashCode => Object.hash(url, scale);
+  int get hashCode => Object.hash(url, scale, webHtmlElementStrategy, headers);
 
   @override
   String toString() =>
-      '${objectRuntimeType(this, 'NetworkImage')}("$url", scale: ${scale.toStringAsFixed(1)})';
+      '${objectRuntimeType(this, 'NetworkImage')}("$url", scale: ${scale.toStringAsFixed(1)}, webHtmlElementStrategy: ${webHtmlElementStrategy.name}, headers: $headers)';
 }
 
 /// An [ImageStreamCompleter] that delegates to another [ImageStreamCompleter]

dev/customer_testing/tests.version
@@ -1,1 +1,1 @@
-7f55026290e721fc648d3811664bbf3abca911d5
+53b1d5666e05e93067104a6345a85c8b33b5d0d7

packages/flutter/lib/src/widgets/_web_image_web.dart
@@ -152,6 +152,7 @@ class RenderWebImage extends RenderShiftedBox {
 
   Alignment? _resolvedAlignment;
   bool? _flipHorizontally;
+  bool _needsClip = false;
 
   void _resolve() {
     if (_resolvedAlignment != null) {
@@ -164,7 +165,7 @@ class RenderWebImage extends RenderShiftedBox {
   void _markNeedResolution() {
     _resolvedAlignment = null;
     _flipHorizontally = null;
-    markNeedsPaint();
+    markNeedsLayout();
   }
 
   /// Whether to paint the image in the direction of the [TextDirection].
@@ -266,7 +267,7 @@ class RenderWebImage extends RenderShiftedBox {
       return;
     }
     _fit = value;
-    markNeedsPaint();
+    markNeedsLayout();
   }
 
   /// How to align the image within its bounds.
@@ -350,19 +351,95 @@ class RenderWebImage extends RenderShiftedBox {
       return;
     }
 
+    // Size and position the <img> element child.
+    //
+    // The `ImgElementPlatformView` which contains the `<img>` element is sized
+    // independently of the `RenderWebImage`. The `RenderWebImage` needs to
+    // size and position its child to match the `BoxFit` and `Alignment`
+    // properties.
+    //
+    // The child could be smaller or larger than the `RenderWebImage`. If the
+    // child is larger, then it will be clipped in the `paint` step.
     final Size inputSize = Size(image.naturalWidth.toDouble(), image.naturalHeight.toDouble());
-    fit ??= BoxFit.scaleDown;
-    final FittedSizes fittedSizes = applyBoxFit(fit!, inputSize, size);
-    final Size childSize = fittedSizes.destination;
-    child!.layout(BoxConstraints.tight(childSize));
-    final double halfWidthDelta = (size.width - childSize.width) / 2.0;
-    final double halfHeightDelta = (size.height - childSize.height) / 2.0;
-    final double dx =
-        halfWidthDelta +
-        (_flipHorizontally! ? -_resolvedAlignment!.x : _resolvedAlignment!.x) * halfWidthDelta;
-    final double dy = halfHeightDelta + _resolvedAlignment!.y * halfHeightDelta;
-    final BoxParentData childParentData = child!.parentData! as BoxParentData;
-    childParentData.offset = Offset(dx, dy);
+    final BoxFit resolvedFit = fit ?? BoxFit.scaleDown;
+    final FittedSizes fittedSizes = applyBoxFit(resolvedFit, inputSize, size);
+
+    if (fittedSizes.source == inputSize) {
+      // If `fittedSizes.source` is the entire original image, then the child
+      // can be sized to `fittedSizes.destination` because there will be no
+      // clipping and the child will be exactly the destination size.
+      final Size childSize = fittedSizes.destination;
+      child!.layout(BoxConstraints.tight(childSize));
+
+      // In case the `childSize` is smaller than the layout constraints (for
+      // example, if the `fit` is `BoxFit.fitWidth` and the resulting child
+      // size causes it to "letterbox" in the containing box) then the child
+      // needs to be offset in the containing box.
+      final double halfWidthDelta = (size.width - childSize.width) / 2.0;
+      final double halfHeightDelta = (size.height - childSize.height) / 2.0;
+      final double dx =
+          halfWidthDelta +
+          (_flipHorizontally! ? -_resolvedAlignment!.x : _resolvedAlignment!.x) * halfWidthDelta;
+      final double dy = halfHeightDelta + _resolvedAlignment!.y * halfHeightDelta;
+      final BoxParentData childParentData = child!.parentData! as BoxParentData;
+      childParentData.offset = Offset(dx, dy);
+      _needsClip = false;
+    } else {
+      // Otherwise, the destination box only shows a portion of the image and
+      // the rest of the image overflows out of the box. The size of the child
+      // needs to be worked out from the size of the source and destination
+      // boxes. The offset of the child needs to be worked out from the
+      // alignment. In the `paint` method, a clip will be applied so that only
+      // the portion of the child that is in the destination box will be shown.
+      final Size sourceSize = fittedSizes.source;
+      final Size destinationSize = fittedSizes.destination;
+
+      // Only `BoxFit.fill` distorts the aspect ratio of the image. The rest of
+      // the `BoxFit` values preserve the aspect ratio. Furthermore, if the
+      // `fit` is `BoxFit.fill` then `fittedSizes.source == inputSize` since the
+      // entire image is drawn into the box. So the case of `BoxFit.fill` is
+      // never hit in this branch of the conditional. Therefore, the child
+      // size should be the source size multiplied by a scalar value. The scale
+      // can be determined by examining the relative sizes of the `sourceSize`
+      // and `destinationSize`. Since aspect ratio is preserved, `sourceSize`
+      // and `destinationSize` must have the same aspect ratio. Therefore, the
+      // scale to multiply the `inputSize` to get the `childSize` is equal to
+      // the ratio of the `destinationSize` to the `sourceSize`.
+      assert(
+        (sourceSize.aspectRatio - destinationSize.aspectRatio).abs() < precisionErrorTolerance,
+      );
+      final double scale = destinationSize.width / sourceSize.width;
+      final Size childSize = inputSize * scale;
+      child!.layout(BoxConstraints.tight(childSize));
+
+      // Now that the child has been sized, it must also be offset by an
+      // appropriate amount so that the specified portion of the image is shown
+      // in the destination box. `Alignment.inscribe` gives the sub-rectangle
+      // of source size that will be shown in the destination box. Again, the
+      // source and destination box have the same aspect ratio, so to compute
+      // the offset for the child, first compute the offset that the original
+      // sized image would need to put the sub-rectangle given by
+      // `Alignment.inscribe` at the origin, and then scale that offset by the
+      // same scale computed above to get the offset for the child.
+      final Rect sourceRect = _resolvedAlignment!.inscribe(sourceSize, Offset.zero & inputSize);
+      final Offset childOffset = Offset(-sourceRect.left, -sourceRect.top) * scale;
+      final BoxParentData childParentData = child!.parentData! as BoxParentData;
+      childParentData.offset = childOffset;
+      _needsClip = true;
+    }
+  }
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    if (child == null) {
+      return;
+    }
+    if (_needsClip) {
+      final Rect destinationRect = Offset.zero & size;
+      context.pushClipRect(needsCompositing, offset, destinationRect, super.paint);
+    } else {
+      super.paint(context, offset);
+    }
   }
 
   @override

packages/flutter/test/painting/_network_image_test_web.dart
@@ -8,7 +8,8 @@ import 'dart:ui_web' as ui_web;
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' hide NetworkImage;
-import 'package:flutter/src/painting/_network_image_web.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/src/painting/_network_image_web.dart' hide NetworkImage;
 import 'package:flutter/src/painting/_web_image_info_web.dart';
 import 'package:flutter/src/web.dart' as web_shim;
 import 'package:flutter/src/widgets/_web_image_web.dart';
@@ -468,6 +469,147 @@ void runTests() {
     expect(imgElement.src, testImg.src);
     expect(imgElement.style.width, '100%');
     expect(imgElement.style.height, '100%');
+  });
+
+  group('RenderWebImage', () {
+    testWidgets('BoxFit.contain centers and sizes the image correctly', (
+      WidgetTester tester,
+    ) async {
+      final TestImgElement testImg = TestImgElement();
+      testImg
+        ..src = _uniqueUrl(tester.testDescription)
+        ..naturalWidth = 200
+        ..naturalHeight = 100;
+      final WebImageInfo image = WebImageInfo(testImg.getMock() as web_shim.HTMLImageElement);
+      await tester.pumpWidget(
+        Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: RawWebImage(image: image, fit: BoxFit.contain),
+          ),
+        ),
+      );
+
+      final RenderWebImage renderWebImage = tester.renderObject(find.byType(RawWebImage));
+      expect(renderWebImage.size, const Size(300, 300));
+
+      final RenderBox child = renderWebImage.child!;
+      expect(child.size, const Size(300, 150));
+
+      final BoxParentData parentData = child.parentData! as BoxParentData;
+      expect(parentData.offset, const Offset(0, 75));
+    });
+
+    testWidgets('BoxFit.cover sizes and clips the image correctly', (WidgetTester tester) async {
+      final TestImgElement testImg = TestImgElement();
+      testImg
+        ..src = _uniqueUrl(tester.testDescription)
+        ..naturalWidth = 200
+        ..naturalHeight = 100;
+      final WebImageInfo image = WebImageInfo(testImg.getMock() as web_shim.HTMLImageElement);
+      await tester.pumpWidget(
+        RepaintBoundary(
+          child: Center(
+            child: SizedBox(
+              width: 300,
+              height: 300,
+              child: RawWebImage(image: image, fit: BoxFit.cover, alignment: Alignment.bottomRight),
+            ),
+          ),
+        ),
+      );
+
+      // Pump and settle so the layer tree updates.
+      await tester.pumpAndSettle();
+
+      final RenderWebImage renderWebImage = tester.renderObject(find.byType(RawWebImage));
+      expect(renderWebImage.size, const Size(300, 300));
+
+      final RenderBox child = renderWebImage.child!;
+      expect(child.size, const Size(600, 300));
+
+      final BoxParentData parentData = child.parentData! as BoxParentData;
+      expect(parentData.offset, const Offset(-300, 0));
+
+      expect(tester.layers, contains(isA<ClipRectLayer>()));
+      final ClipRectLayer clipLayer = tester.layers.whereType<ClipRectLayer>().first;
+      expect(clipLayer.clipRect, const Rect.fromLTWH(250, 150, 300, 300));
+    });
+
+    testWidgets('BoxFit.none does not scale and clips when necessary', (WidgetTester tester) async {
+      final TestImgElement testImg = TestImgElement();
+      testImg
+        ..src = _uniqueUrl(tester.testDescription)
+        ..naturalWidth = 200
+        ..naturalHeight = 100;
+      final WebImageInfo image = WebImageInfo(testImg.getMock() as web_shim.HTMLImageElement);
+      await tester.pumpWidget(
+        RepaintBoundary(
+          child: Center(
+            child: SizedBox(
+              width: 100,
+              height: 50,
+              child: RawWebImage(image: image, fit: BoxFit.none, alignment: Alignment.topLeft),
+            ),
+          ),
+        ),
+      );
+
+      // Pump and settle so the layer tree updates.
+      await tester.pumpAndSettle();
+
+      final RenderWebImage renderWebImage = tester.renderObject(find.byType(RawWebImage));
+      expect(renderWebImage.size, const Size(100, 50));
+
+      final RenderBox child = renderWebImage.child!;
+      expect(child.size, const Size(200, 100));
+
+      final BoxParentData parentData = child.parentData! as BoxParentData;
+      expect(parentData.offset, Offset.zero);
+
+      expect(tester.layers, contains(isA<ClipRectLayer>()));
+    });
+
+    testWidgets('Alignment works correctly with various BoxFit values', (
+      WidgetTester tester,
+    ) async {
+      final TestImgElement testImg = TestImgElement();
+      testImg
+        ..src = _uniqueUrl(tester.testDescription)
+        ..naturalWidth = 200
+        ..naturalHeight = 100;
+      final WebImageInfo image = WebImageInfo(testImg.getMock() as web_shim.HTMLImageElement);
+      await tester.pumpWidget(
+        Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: RawWebImage(image: image, fit: BoxFit.contain, alignment: Alignment.topLeft),
+          ),
+        ),
+      );
+
+      RenderWebImage renderWebImage = tester.renderObject(find.byType(RawWebImage));
+      RenderBox child = renderWebImage.child!;
+      BoxParentData parentData = child.parentData! as BoxParentData;
+      expect(parentData.offset, Offset.zero);
+
+      await tester.pumpWidget(
+        Center(
+          child: SizedBox(
+            width: 300,
+            height: 300,
+            child: RawWebImage(image: image, fit: BoxFit.contain, alignment: Alignment.bottomRight),
+          ),
+        ),
+      );
+
+      renderWebImage = tester.renderObject(find.byType(RawWebImage));
+      child = renderWebImage.child!;
+      parentData = child.parentData! as BoxParentData;
+      expect(parentData.offset, const Offset(0, 150));
+    });
   });
 }
 

packages/flutter/test/painting/_test_http_request.dart
@@ -123,8 +123,8 @@ class TestImgElement {
   late ImgElementMock _mock;
 
   String src = '';
-  int naturalWidth = -1;
-  int naturalHeight = -1;
+  int naturalWidth = 1;
+  int naturalHeight = 1;
 
   // Either `decode` or `decodeSuccess/Failure` may be called first.
   // The following fields allow properly handling either case.

packages/flutter/test/painting/image_provider_test.dart
@@ -184,7 +184,10 @@ void main() {
   }, skip: kIsWeb); // [intended] The web cannot load files.
 
   test('ImageProvider toStrings', () async {
-    expect(const NetworkImage('test', scale: 1.21).toString(), 'NetworkImage("test", scale: 1.2)');
+    expect(
+      const NetworkImage('test', scale: 1.21).toString(),
+      'NetworkImage("test", scale: 1.2, webHtmlElementStrategy: never, headers: null)',
+    );
     expect(
       const ExactAssetImage('test', scale: 1.21).toString(),
       'ExactAssetImage(name: "test", scale: 1.2, bundle: null)',

commit cf0805284a5e1bea32c6b84257cc414ecff3162c

Fix Image.network not using cache when headers are specified (#176831)
Fix a bug introduced by bb116cb55 (#171916), where `hashCode` is giving
different results for values which `==` says are equal. Which broke
caching for uses of `Image.network` widget when `headers` map was
passed.

## Pre-launch Checklist

- [x] I read the [Contributor Guide] and followed the process outlined
there for submitting PRs.
- [x] I read the [Tree Hygiene] wiki page, which explains my
responsibilities.
- [x] I read and followed the [Flutter Style Guide], including [Features
we expect every widget to implement].
- [x] I signed the [CLA].
- [ ] I listed at least one issue that this PR fixes in the description
above.
- [x] I updated/added relevant documentation (doc comments with `///`).
- [x] I added new tests to check the change I am making, or this PR is
[test-exempt].
- [x] I followed the [breaking change policy] and added [Data Driven
Fixes] where supported.
- [x] All existing and new tests are passing.

If you need help, consider asking for advice on the #hackers-new channel
on [Discord].

**Note**: The Flutter team is currently trialing the use of [Gemini Code
Assist for
GitHub](https://developers.google.com/gemini-code-assist/docs/review-github-code).
Comments from the `gemini-code-assist` bot should not be taken as
authoritative feedback from the Flutter team. If you find its comments
useful you can update your code accordingly, but if you are unsure or
disagree with the feedback, please feel free to wait for a Flutter team
member's review for guidance on which automated comments should be
addressed.

<!-- Links -->
[Contributor Guide]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#overview
[Tree Hygiene]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md
[test-exempt]:
https://github.com/flutter/flutter/blob/main/docs/contributing/Tree-hygiene.md#tests
[Flutter Style Guide]:

packages/flutter/lib/src/painting/_network_image_io.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:ui' as ui;
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 
 import 'binding.dart';
@@ -171,7 +172,7 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
   }
 
   @override
-  int get hashCode => Object.hash(url, scale, headers);
+  int get hashCode => Object.hash(url, scale, const MapEquality<String, String>().hash(headers));
 
   @override
   String toString() =>

packages/flutter/lib/src/painting/_network_image_web.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 import 'dart:js_interop';
 import 'dart:ui' as ui;
 
+import 'package:collection/collection.dart';
 import 'package:flutter/foundation.dart';
 
 import '../web.dart' as web;
@@ -250,7 +251,12 @@ class NetworkImage extends image_provider.ImageProvider<image_provider.NetworkIm
   }
 
   @override
-  int get hashCode => Object.hash(url, scale, webHtmlElementStrategy, headers);
+  int get hashCode => Object.hash(
+    url,
+    scale,
+    webHtmlElementStrategy,
+    const MapEquality<String, String>().hash(headers),
+  );
 
   @override
   String toString() =>

packages/flutter/test/painting/image_provider_network_image_test.dart
@@ -288,6 +288,51 @@ void main() {
     debugNetworkImageHttpClientProvider = null;
   }, skip: isBrowser); // [intended] Browser does not resolve images this way.
 
+  test('Network image with same arguments uses cache', () async {
+    final _FakeHttpClient mockHttpClient = _FakeHttpClient();
+    debugNetworkImageHttpClientProvider = () => mockHttpClient;
+    mockHttpClient.request.response
+      ..statusCode = HttpStatus.ok
+      ..contentLength = kTransparentImage.length
+      ..content = <Uint8List>[Uint8List.fromList(kTransparentImage)];
+
+    final Completer<void> imageAvailable1 = Completer<void>();
+    final ImageProvider imageProvider1 = NetworkImage(
+      nonconst('testing.url'),
+      headers: nonconst(<String, String>{'key': 'value'}),
+    );
+    expect(imageCache.liveImageCount, 0);
+
+    final ImageStream result1 = imageProvider1.resolve(ImageConfiguration.empty);
+    result1.addListener(
+      ImageStreamListener((ImageInfo image, bool synchronousCall) {
+        imageAvailable1.complete();
+      }),
+    );
+    await imageAvailable1.future;
+    expect(imageCache.liveImageCount, 1);
+
+    // NetworkImage should not make another request.
+    mockHttpClient.request.response.statusCode = HttpStatus.badRequest;
+
+    final Completer<void> imageAvailable2 = Completer<void>();
+    final ImageProvider imageProvider2 = NetworkImage(
+      nonconst('testing.url'),
+      headers: nonconst(<String, String>{'key': 'value'}),
+    );
+
+    final ImageStream result2 = imageProvider2.resolve(ImageConfiguration.empty);
+    result2.addListener(
+      ImageStreamListener((ImageInfo image, bool synchronousCall) {
+        imageAvailable2.complete();
+      }),
+    );
+    await imageAvailable2.future;
+    expect(imageCache.liveImageCount, 1);
+
+    debugNetworkImageHttpClientProvider = null;
+  }, skip: isBrowser); // [intended] Browser does not resolve images this way.
+
   test('Network image sets tag', () async {
     const String url = 'http://test.png';
     const int chunkSize = 8;
@@ -341,6 +386,9 @@ class _FakeHttpClientRequest extends Fake implements HttpClientRequest {
   final _FakeHttpClientResponse response = _FakeHttpClientResponse();
 
   @override
+  final HttpHeaders headers = _FakeHttpHeaders();
+
+  @override
   Future<HttpClientResponse> close() async {
     return response;
   }
@@ -378,4 +426,9 @@ class _FakeHttpClientResponse extends Fake implements HttpClientResponse {
     drained = true;
     return futureValue ?? futureValue as E; // Mirrors the implementation in Stream.
   }
+}
+
+class _FakeHttpHeaders extends Fake implements HttpHeaders {
+  @override
+  void add(String name, Object value, {bool preserveHeaderCase = false}) {}
 }